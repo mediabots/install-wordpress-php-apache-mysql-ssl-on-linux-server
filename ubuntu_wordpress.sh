2def0ae7a80631674fe2e19569a783ea604a4795
mediabots <mediabots@mail.ru>
2024-04-17 14:06:27 +0530

    Apache Reverse Proxy & Load Balancing Support

--- ubuntu_wordpress.sh
+++ ubuntu_wordpress.sh
@@ -184,7 +184,7 @@ dos2unix ./wordpress/wp-config.php
 
 sudo -E apt -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" -qq -y install liblua5.3-0 php-common php8.1-cli php8.1-common php8.1-opcache php8.1-readline php-json php8.1-cgi php8.1-curl php8.1-gd php8.1-mbstring php8.1-xml php8.1-xmlrpc php8.1-soap php8.1-intl php8.1-zip  --allow-change-held-packages
 
-sudo -E apt -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" -qq -y install apache2 libapache2-mod-php8.1 zip php8.1-mysql sendmail --allow-change-held-packages
+sudo -E apt -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" -qq -y install apache2 libapache2-mod-php8.1 libxml2-dev zip php8.1-mysql sendmail --allow-change-held-packages
 
 # configure sendmail
 yes 'y' | sudo sendmailconfig 
@@ -192,6 +192,9 @@ yes 'y' | sudo sendmailconfig
 # configure Apache
 sudo systemctl enable --now apache2
 sudo chown -R "$USER":root /var/www
+
+# enable reverse proxy & load balancing capability for Apache
+sudo a2enmod proxy proxy_http proxy_balancer lbmethod_byrequests
 
 # - prepare Website for Apache
 mkdir /var/www/html/$SITENAME
